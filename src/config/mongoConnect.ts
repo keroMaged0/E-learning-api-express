4628fead33219f1238b2ab9693b93de7788bf2ed
keromaged0 <iti.202020138@thebes.edu.eg>
2024-09-27 21:19:51 +0300

    update

--- src/config/mongoConnect.ts
+++ src/config/mongoConnect.ts
@@ -3,9 +3,7 @@ import { logger } from "./logger";
 
 
 export const connectMongoDB = (url: string) => {
-    mongoose.connect(url).then(() => {
-        console.log("scdlmndcdkcndncdnjdcndjndcjndcjdcndncncnjdcndc");
-        
+    mongoose.connect(url).then(() => {        
         logger.info("Connected to MongoDB")
     }).catch((error) => {
         logger.error("Error connecting to MongoDB", error);
